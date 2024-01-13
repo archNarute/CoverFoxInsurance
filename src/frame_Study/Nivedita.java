69c6b379c01522fe1a710032a888224fd84e6236
NiveditaCPawar <nivipawar1996@gmail.com>
2024-01-13 23:04:23 +0530

    [Nivedita]:hii

---
+++ src/frame_Study/Nivedita.java
@@ -0,0 +1,13 @@
+package frame_Study;
+
+public class Nivedita {
+	
+
+		public static void main(String[] args) {
+			System.out.println("hello this is my new project");
+			System.out.println("hello this is my new project");
+		}
+
+	}
+
+

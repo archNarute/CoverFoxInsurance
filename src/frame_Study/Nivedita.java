416a67fb84a6a6419fb4f672a51669567d25730e
NiveditaCPawar <nivipawar1996@gmail.com>
2024-01-13 23:08:04 +0530

    [Nivedita]:hello

--- src/frame_Study/Nivedita.java
+++ src/frame_Study/Nivedita.java
@@ -6,6 +6,7 @@ public class Nivedita {
 		public static void main(String[] args) {
 			System.out.println("hello this is my new project");
 			System.out.println("hello this is my new project");
+			System.out.println("hello");
 		}
 
 	}

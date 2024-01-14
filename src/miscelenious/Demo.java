fca6b3025f98a2d04fd61294d7d27e9e26366f29
archNarute <narutearchana1997@gmail.com>
2024-01-14 22:01:24 +0530

    [Archana]:new method is added in demo class under miscelenious package

--- src/miscelenious/Demo.java
+++ src/miscelenious/Demo.java
@@ -4,6 +4,11 @@ public class Demo {
 
 	public static void main(String[] args) {
 		System.out.println("work is completed");
+		
+		
+		
+		System.out.println("all task is completed");
+		System.out.println("project work is done");
 
 	}
 
